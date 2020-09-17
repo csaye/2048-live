a5ed8787051a25edb6dee3622cc7cc194df8197a
Cooper Saye <coopersaye@gmail.com>
2020-09-17 14:42:09 -0700

    Implement base of game and renderer classes.

---
+++ src/game.cpp
@@ -0,0 +1,8 @@
+#include "game.h"
+#include "renderer.h"
+#include "renderer.cpp"
+
+Game::Game()
+{
+    Renderer renderer;
+}
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include "game.h"
+#include "game.cpp"
+
+int main()
+{
+    Game game;
+}
