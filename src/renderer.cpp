f9d8f61db21ee4a0853783a7eb6bc0d399d69791
Cooper Saye <coopersaye@gmail.com>
2020-09-17 21:04:10 -0700

    Implement image rendering.

--- src/renderer.cpp
+++ src/renderer.cpp
@@ -2,6 +2,12 @@
 
 #include "renderer.h"
 
+// const Color red(255, 0, 0, 255);
+// const Color green(0, 255, 0, 255);
+// const Color blue(0, 0, 255, 255);
+// const Color white(255, 255, 255, 255);
+// const Color black(0, 0, 0, 255);
+
 Renderer::Renderer(int screenWidth_, int screenHeight_, int boardWidth_, int boardHeight_)
 {
     screenWidth = screenWidth_;
@@ -10,6 +16,7 @@ Renderer::Renderer(int screenWidth_, int screenHeight_, int boardWidth_, int boa
     boardHeight = boardHeight_;
 
     window = SDL_CreateWindow("2048 Live", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, screenWidth, screenHeight, SDL_WINDOW_SHOWN);
+    surface = SDL_GetWindowSurface(window);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
 }
 
@@ -19,47 +26,52 @@ Renderer::~Renderer()
     SDL_Quit();
 }
 
-void Renderer::clear()
-{
-    SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
-    SDL_RenderClear(renderer);
-}
+// void Renderer::clear()
+// {
+//     SDL_SetRenderDrawColor(renderer, 0, 0, 0, 255);
+//     SDL_RenderClear(renderer);
+// }
 
-void Renderer::drawRect(Rect rect, Color color)
-{
-    // std::cout << "rect at (" << rect.x << ", " << rect.y << ") of size (" << rect.w << ", " << rect.h << ")";
+// void Renderer::drawRect(SDL_Rect rect, SDL_Color color)
+// {
+//     SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
+//     SDL_RenderFillRect(renderer, &rect);
+// }
 
-    SDL_Rect sdlRect{rect.x, rect.y, rect.w, rect.h};
-    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
-    SDL_RenderFillRect(renderer, &sdlRect);
+void Renderer::loadImage(std::string name, SDL_Rect rect)
+{
+    // std::string path = "testingPic.bmp";
+    // SDL_Surface *image = SDL_LoadBMP(path.c_str());
+    SDL_Surface *image = SDL_LoadBMP("test.bmp");
+    if (image == NULL) std::cout << "null image";
+    SDL_BlitSurface(image, &rect, surface, NULL);
 }
 
 void Renderer::render(std::vector<std::vector<int>> &board)
 {
     std::cout << "rendering\n";
 
-    clear();
+    // clear();
 
     int w = screenWidth / boardWidth;
     int h = screenHeight / boardHeight;
-
-    Color black(0, 0, 0, 255);
-    Color white(255, 255, 255, 255);
 
     for (int x = 0; x < board.size(); x++)
     {
         for (int y = 0; y < board[x].size(); y++)
         {
-            Rect rect(x * w, y * h, w, h);
+            SDL_Rect rect{x * w, y * h, w, h};
 
             switch (board[x][y])
             {
                 case 1:
-                    drawRect(rect, white);
+                    // drawRect(rect, white);
+                    loadImage("testingPic", rect);
                     break;
             }
         }
     }
 
+    SDL_UpdateWindowSurface(window);
     SDL_RenderPresent(renderer);
 }
--- src/renderer.h
+++ src/renderer.h
@@ -3,56 +3,6 @@
 
 #include <vector>
 
-struct Rect
-{
-    Rect(int x_, int y_, int w_, int h_)
-    {
-        if (x_ < 0) x = 0;
-        else x = x_;
-        
-        if (y_ < 0) y = 0;
-        else y = y_;
-        
-        if (w_ < 0) w = 0;
-        else w = w_;
-
-        if (h_ < 0) h = 0;
-        else h = h_;
-    }
-
-    int x;
-    int y;
-    int w;
-    int h;
-};
-
-struct Color
-{
-    Color(int r_, int g_, int b_, int a_)
-    {
-        if (r_ < 0) r = 0;
-        else if (r_ > 255) r = 255;
-        else r = r_;
-
-        if (g_ < 0) g = 0;
-        else if (g_ > 255) g = 255;
-        else g = g_;
-
-        if (b_ < 0) b = 0;
-        else if (b_ > 255) b = 255;
-        else b = b_;
-
-        if (a_ < 0) a = 0;
-        else if (a_ > 255) a = 255;
-        else a = a_;
-    }
-
-    int r;
-    int g;
-    int b;
-    int a;
-};
-
 class Renderer
 {
     public:
@@ -65,9 +15,12 @@ class Renderer
         int boardWidth;
         int boardHeight;
         SDL_Window *window;
+        SDL_Surface *surface;
         SDL_Renderer *renderer;
-        void clear();
-        void drawRect(Rect rect, Color color);
+        
+        // void clear();
+        // void drawRect(SDL_Rect rect, SDL_Color color);
+        void loadImage(std::string name, SDL_Rect rect);
 };
 
 #endif
